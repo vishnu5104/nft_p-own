ea1c7f156517db3d1de4fe11bdc3853b9c5654ff
vishnu5104 <knp19cs044@ceknpy.ac.in>
2022-07-16 13:51:46 +0530

    add

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,5 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const MarketAddress = '0x173714E8bA7E80399656Cc9B4B7741cb0Ddf83f4';
 export const MarketAddressABI = market.abi;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,28 @@
-const fs = require('fs');
-// eslint-disable-next-line import/no-extraneous-dependencies
-require('@nomiclabs/hardhat-waffle');
+// const fs = require('fs');
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// require('@nomiclabs/hardhat-waffle');
 
-const privatekey = fs.readFileSync('.secret').toString().trim();
+// const privatekey = fs.readFileSync('.secret').toString().trim();
+
+// module.exports = {
+//   networks: {
+//     hardhat: {
+//       chainId: 1337,
+//     },
+
+//   },
+//   solidity: '0.8.4',
+// };
+
+require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   networks: {
-    hardhat: {
-      chainId: 1337,
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/U3iiPIsSHCWuVkjBQODZQFlLFaaZ_QHu',
+      accounts: ['34dcd36d7af393c3d32d1a1667dcdaeea1f624517c1a4a72300e088c93530eb2'],
     },
 
   },
   solidity: '0.8.4',
 };
-
