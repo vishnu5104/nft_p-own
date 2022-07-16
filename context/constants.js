2b6c34f53a5a2fedba082903e20e39d5fb5578f6
vishnu5104 <knp19cs044@ceknpy.ac.in>
2022-07-16 14:08:44 +0530

    adnew

--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,5 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0x173714E8bA7E80399656Cc9B4B7741cb0Ddf83f4';
+export const MarketAddress = '0x858D36684ca81CFCEdB8127d0fe257bCf65ef3C7';
 export const MarketAddressABI = market.abi;
 
